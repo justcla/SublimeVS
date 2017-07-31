fc5d77c8e00d83ca057c72e8e8a24ac6cfacd97e
Justin Clareburt <justcla@microsoft.com>
2017-07-30 17:53:00 -0700

    Moved all settings code to VSSettingsManager

--- VSSetingsManager/VSSettingsManager.cs
+++ VSSetingsManager/VSSettingsManager.cs
@@ -3,6 +3,13 @@ using System.ComponentModel.Design;
 using System.Globalization;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using System.Windows.Forms;
+using Microsoft.VisualStudio;
+using System.IO;
+using System.Reflection;
+using EnvDTE80;
+using EnvDTE;
+using System.Diagnostics;
 
 namespace VSSetingsManager
 {
@@ -26,6 +33,8 @@ namespace VSSetingsManager
         /// </summary>
         private readonly Package package;
 
+        private const string SublimeSettingsFileName = @"Shortcuts\SublimeShortcuts.vssettings";
+
         /// <summary>
         /// Initializes a new instance of the <see cref="VSSettingsManager"/> class.
         /// Adds our command handlers for menu (commands must exist in the command table file)
@@ -33,15 +42,10 @@ namespace VSSetingsManager
         /// <param name="package">Owner package, not null.</param>
         private VSSettingsManager(Package package)
         {
-            if (package == null)
-            {
-                throw new ArgumentNullException("package");
-            }
+            // Register this command with the Global Command Service
+            this.package = package ?? throw new ArgumentNullException("package");
 
-            this.package = package;
-
-            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (commandService != null)
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
             {
                 var menuCommandID = new CommandID(CommandSet, CommandId);
                 var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
@@ -87,17 +91,92 @@ namespace VSSetingsManager
         /// <param name="e">Event args.</param>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "VSSettingsManager";
-
-            // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.ServiceProvider,
-                message,
-                title,
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            //ShowDefaultAlert();
+            ApplySublimeShortcuts();
         }
+
+        //------------ Shortcut Settings --------------
+
+        public void ApplySublimeShortcuts()
+        {
+            //Ask user if they want to apply Sublime settings
+            if (ConfirmApplyShortcuts())
+            {
+                ImportUserSettings(SublimeSettingsFileName);
+            }
+        }
+
+        private static bool ConfirmApplyShortcuts()
+        {
+            const string title = "Extensions for Accessibility in Visual Studio";
+            const string message =
+                "Congratulations! Sublime VS Extension has been installed.\n" +
+                "\n" +
+                "Apply the following settings:\n" +
+                "- Turn on Map Mode Scrollbar (Wide)\n" +
+                "- Apply Sublime Text keyboard shortcuts\n" +
+                "\n" +
+                "Note: You can modify these settings later in Tools->Options";
+            return MessageBox.Show(message, title, MessageBoxButtons.OKCancel) == DialogResult.OK;
+        }
+
+        private void ImportUserSettings(string settingsFileName)
+        {
+            if (ServiceProvider.GetService(typeof(SVsUIShell)) is IVsUIShell shell)
+            {
+                // import the settings file into Visual Studio
+                var asmDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var settingsFilePath = Path.Combine(asmDirectory, settingsFileName);
+                var group = VSConstants.CMDSETID.StandardCommandSet2K_guid;
+
+                object arguments = string.Format(CultureInfo.InvariantCulture, "-import:\"{0}\"", settingsFilePath);
+                shell.PostExecCommand(ref group, (uint)VSConstants.VSStd2KCmdID.ManageUserSettings, 0, ref arguments);
+            }
+        }
+
+        //------------ MiniMap settings ------------
+
+        public void ApplyMiniMap()
+        {
+            // Confirm with user before applying MiniMap
+            if (ConfirmApplyMinimap())
+            {
+                ApplyMapModeScrollbar();
+            }
+
+        }
+
+        private static bool ConfirmApplyMinimap()
+        {
+            const string title = "Extensions for Accessibility in Visual Studio";
+            const string message =
+                "Apply the following settings:\n" +
+                "- Turn on Map Mode Scrollbar (Wide)\n" +
+                "\n" +
+                "Note: You can modify these settings later in Tools->Options";
+            return MessageBox.Show(message, title, MessageBoxButtons.OKCancel) == DialogResult.OK;
+        }
+
+        private void ApplyMapModeScrollbar()
+        {
+            try
+            {
+                var dte2 = (DTE2)(ServiceProvider.GetService(typeof(DTE)));
+                UpdateSetting(dte2, "TextEditor", "AllLanguages", "UseMapMode", true);
+                UpdateSetting(dte2, "TextEditor", "AllLanguages", "OverviewWidth", (short)83);
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+            }
+        }
+
+
+        private static void UpdateSetting(DTE2 dte2, string category, string page, string settingName, object value)
+        {
+            // Example: dte2.Properties["TextEditor", "General"].Item("TrackChanges").Value = true;
+            dte2.Properties[category, page].Item(settingName).Value = value;
+        }
+
     }
 }
--- VSSetingsManager/VSSettingsManagerPackage.cs
+++ VSSetingsManager/VSSettingsManagerPackage.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.ComponentModel.Design;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Runtime.InteropServices;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Microsoft.Win32;
+using Microsoft.VisualStudio.Settings;
+using Microsoft.VisualStudio;
 
 namespace VSSetingsManager
 {
@@ -33,6 +28,7 @@ namespace VSSetingsManager
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(VSSettingsManagerPackage.PackageGuidString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     public sealed class VSSettingsManagerPackage : Package
     {
@@ -40,6 +36,8 @@ namespace VSSetingsManager
         /// VSSettingsManagerPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "2145fd5c-c814-4772-b19d-b840113afede";
+        private const string SID_SVsSettingsPersistenceManager = "9B164E40-C3A2-4363-9BC5-EB4039DEF653";
+        public static ISettingsManager SettingsManager { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="VSSettingsManager"/> class.
@@ -60,10 +58,31 @@ namespace VSSetingsManager
         /// </summary>
         protected override void Initialize()
         {
+            SettingsManager = (ISettingsManager)GetGlobalService(typeof(SVsSettingsPersistenceManager));
             VSSettingsManager.Initialize(this);
             base.Initialize();
+
+            // Check if we need to do first-time setup
+            const string firstTimeRunSettingName = "SublimeSettingsPrompted";
+            if ((VSSettingsManagerPackage.SettingsManager.TryGetValue(firstTimeRunSettingName, out bool value) != GetValueResult.Success) || !value)
+            {
+                VSSettingsManagerPackage.SettingsManager.SetValueAsync(firstTimeRunSettingName, true, isMachineLocal: true);
+                ApplFirstTimeSettings();
+
+            }
+        }
+
+        private static void ApplFirstTimeSettings()
+        {
+            // Apply First-time settings
+            VSSettingsManager.Instance.ApplyMiniMap();
+            VSSettingsManager.Instance.ApplySublimeShortcuts();
         }
 
         #endregion
+        // A horrible hack but SVsSettingsPersistenceManager isn't public and we need something with the right GUID to get the service.
+        [Guid(SID_SVsSettingsPersistenceManager)]
+        private class SVsSettingsPersistenceManager
+        { }
     }
 }
