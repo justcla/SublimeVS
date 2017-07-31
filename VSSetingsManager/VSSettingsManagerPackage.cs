17ff34b93f5983ca9defd6899cfbc0aec36343ef
Justin Clareburt <justcla@microsoft.com>
2017-07-30 22:12:53 -0700

    Commented out first-run behaviour

--- VSSetingsManager/VSSettingsManagerPackage.cs
+++ VSSetingsManager/VSSettingsManagerPackage.cs
@@ -7,23 +7,6 @@ using Microsoft.VisualStudio;
 
 namespace VSSetingsManager
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the
-    /// IVsPackage interface and uses the registration attributes defined in the framework to
-    /// register itself and its components with the shell. These attributes tell the pkgdef creation
-    /// utility what data to put into .pkgdef file.
-    /// </para>
-    /// <para>
-    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
-    /// </para>
-    /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
@@ -39,15 +22,8 @@ namespace VSSetingsManager
         private const string SID_SVsSettingsPersistenceManager = "9B164E40-C3A2-4363-9BC5-EB4039DEF653";
         public static ISettingsManager SettingsManager { get; private set; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="VSSettingsManager"/> class.
-        /// </summary>
         public VSSettingsManagerPackage()
         {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
         }
 
         #region Package Members
@@ -58,18 +34,20 @@ namespace VSSetingsManager
         /// </summary>
         protected override void Initialize()
         {
+            // Initialize settings manager (TODO: could be done lazily on get)
             SettingsManager = (ISettingsManager)GetGlobalService(typeof(SVsSettingsPersistenceManager));
+
+            // Adds commands handlers for the VS Settings operations (Apply, Backup, Restore, Reset)
             VSSettingsManager.Initialize(this);
             base.Initialize();
 
             // Check if we need to do first-time setup
-            const string firstTimeRunSettingName = "SublimeSettingsPrompted";
-            if ((VSSettingsManagerPackage.SettingsManager.TryGetValue(firstTimeRunSettingName, out bool value) != GetValueResult.Success) || !value)
-            {
-                VSSettingsManagerPackage.SettingsManager.SetValueAsync(firstTimeRunSettingName, true, isMachineLocal: true);
-                ApplFirstTimeSettings();
-
-            }
+            //const string firstTimeRunSettingName = "SublimeSettingsPrompted";
+            //if ((VSSettingsManagerPackage.SettingsManager.TryGetValue(firstTimeRunSettingName, out bool value) != GetValueResult.Success) || !value)
+            //{
+            //    VSSettingsManagerPackage.SettingsManager.SetValueAsync(firstTimeRunSettingName, true, isMachineLocal: true);
+            //    ApplFirstTimeSettings();
+            //}
         }
 
         private static void ApplFirstTimeSettings()
